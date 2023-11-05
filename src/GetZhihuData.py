8709e09074422387744e3a7655750b1d7ff69dfe
guofei9987 <guofei9987@foxmail.com>
2023-11-05 15:15:43 +0800

    关注者

--- src/GetZhihuData.py
+++ src/GetZhihuData.py
@@ -26,7 +26,7 @@ print(stars[0].text)  # 点赞、喜欢、收藏
 # 关注者数量
 follows = soup.find_all(name='strong', attrs={'class': 'NumberBoard-itemValue'})
 print(follows[1].text)  # 关注
-follows_num=follows[1].text
+follows_num = follows[1].text
 
 agree, like, collection = re.findall('[0-9,]+', stars[0].text)
 
@@ -49,7 +49,7 @@ def num2k(x):
 achievement['zhihu_agree_str'] = num2k(agree)
 achievement['zhihu_like_str'] = num2k(like)
 achievement['zhihu_collection_str'] = num2k(collection)
-achievement['zhihu_follows_str'] = num2k(follows[1].text)
+achievement['zhihu_follows_str'] = follows_num  # num2k(follows[1].text)
 
 with open('achievement.json', 'w') as f:
     json.dump(achievement, f, ensure_ascii=False, indent='')
